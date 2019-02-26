b049e8123cf852d8a643dd9c5a26ca435d1c757a
Liuzhongqing <1091213401@qq.cpm>
2019-02-26 09:09:35 +0800

    axios

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="wrapper">
-    <swiper :options="swiperOption" >
-      <swiper-slide v-for="item of swiperList" :key="item.id">
+    <swiper :options="swiperOption" v-if="showSwiper">
+      <swiper-slide v-for="item of list" :key="item.id">
         <img class="swiper-img"  :src="item.imgUrl" />
       </swiper-slide>
       <div class="swiper-pagination"  slot="pagination"></div>
@@ -12,26 +12,20 @@
 <script>
 export default {
   name: 'HomeSwiper',
+  props: {
+    list: Array
+  },
   data () {
     return {
       swiperOption: {
         pagination: '.swiper-pagination',
         loop: true
       },
-      swiperList: [{
-        id: '0001',
-        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1808/a0/980b19eda13bb202.jpg_890x330_d0cc76d2.jpg'
-      }, {
-        id: '0002',
-        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1812/62/c4377b2e1540d202.jpg_890x330_9f2fcdfb.jpg'
-      }, {
-        id: '0003',
-        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1712/d0/0f5f1844045c0102.jpg_890x330_68cb5860.jpg'
-      }, {
-        id: '0004',
-        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1901/ad/67da41581c879402.jpg_890x330_e23bdca1.jpg'
+      computed: {
+        showSwiper () {
+          return this.list.length
+        }
       }
-      ]
     }
   }
 }
@@ -40,8 +34,6 @@ export default {
 <style lang="stylus" scoped>
   .wrapper >>> .swiper-pagination-bullet-active
                 background: #fff
-  .wrapper >>> .swiper-pagination-bullets
-                bottom: .44rem
   .wrapper
     overflow: hidden
     width: 100%
